59cf79d5a62db0ed8a765abf53d539720f43951d
Charles Pao <windperson@gmail.com>
2021-05-14 17:54:44 +0800

    Add project files.

---
+++ XamarinFormsWidgetDemo/XamarinFormsWidgetDemo/Views/ItemDetailPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel;
+using Xamarin.Forms;
+using XamarinFormsWidgetDemo.ViewModels;
+
+namespace XamarinFormsWidgetDemo.Views
+{
+    public partial class ItemDetailPage : ContentPage
+    {
+        public ItemDetailPage()
+        {
+            InitializeComponent();
+            BindingContext = new ItemDetailViewModel();
+        }
+    }
+}
